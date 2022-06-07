17319421c27a5b6d2a8a4eede9745638458735a6
Anderso Henrique <dantasanderson57@hotmail.com>
2022-06-06 21:01:16 -0300

    Àrvore binaria

--- src/arvoresbinaria.js
+++ src/arvoresbinaria.js
@@ -49,6 +49,42 @@ class avlbinaria{
             }
         }
     }
+
+    Remover(dado){
+        this.raiz = this.RemoverNo(this.raiz, dado);
+    }
+
+    RemoverNo(Node, key){
+        if (Node === null){
+            return null;
+        } else if (key > Node.dado){
+            Node.direita = this.RemoverNo(Node.direita, key);
+            return Node;
+        } else {
+            if (Node.esquerda === null && Node.direita === null){
+                Node = null;
+                return Node;
+            } 
+            if(Node.esquerda === null){
+                Node = Node.direita;
+                return Node;
+            } else if (Node.direita === null){
+                Node = Node.esquerda;
+                return Node;
+            }
+            let aux = this.EncontrarMenorNo(Node.direita);
+            Node.dado = aux.dado;
+            Node.direita = this.RemoverNo(Node.direita, aux.dado);
+            return Node;
+        }
+    }
+    EncontrarMenorNo(Node){
+        if (Node.esquerda===null){
+            return Node;
+        } else {
+            return this.EncontrarMenorNo(Node.esquerda);
+        }
+    }
 }
 
 
